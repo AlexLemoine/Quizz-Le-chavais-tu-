0cbc90c1b2939e2be7892889c1d77b450d554e44
AlexLemoine <alexandra.claire1409@gmail.com>
2022-12-15 10:59:27 +0100

    ajout ordre aléatoire des questions et réponses

--- js/main.js
+++ js/main.js
@@ -1,20 +1,59 @@
+// *****************************
 // ********* FONCTIONS *********
 // *****************************
 
+// Trier les réponses dans un ordre aléatoire
+function shuffle(array)
+{
+    // Créer une copie du tableau array pour ne pas perdre l'ordre du tableau d'origine après l'affichage en aléatoire
+    
+    let arrayCopy = [];
+    for (i = 0; i < array.length; i++) {
+    arrayCopy[i] = array[i];
+    }
+
+    // Mélanger la copie du tableau
+    for (let i = arrayCopy.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+    };   
+
+    // Retourner la copie du tableau
+    return arrayCopy;    
+};
 
 
+// Y appliquer la fonction shuffle, à la fois pour les questions et pour les réponses de chaque question
 
+function toHTML(question)
+{   
+    let answersCopy=shuffle(question.answers);
+    let html =
+        `<fieldset class="questions">
+        <legend class="legend">${question.content}</legend>
+        `;
+    for(let indice=0; indice<answersCopy.length;indice++){
+        html+=
+            `<div class="answers">
+            <input type="radio" name="" id="answer"> 
+            <label for="answer">${answersCopy[indice]}</label>
+            </div>`;
+    };
+    html+=
+        `<p class="hidden">${question.comment}</p>
+        </fieldset>`;
+    const form=document.body.querySelector("#form");
+    form.innerHTML+=html;
+};
+
+
+// *****************************
 // ********* VARIABLES *********
 // *****************************
 
 // DE QUOI AI-JE BESOIN ?
-// Des questions + des réponses
 
-// A faire:
-// Des boutons radio devant chaque réponse
-// Un bouton "submit"
-// Placer un écouteur sur les boutons radio
-// Placer un écouteur sur le bouton "submit"
+// Des questions + des réponses
 
 let questions = [
     {
@@ -49,10 +88,9 @@ let questions = [
     }
 ];
 
-// CREER UNE CLASSE
-// Y CONSTRUIRE LE CODE HTML DES QUESTIONS+REPONSES
-
-// A faire:
+// Créer une classe
+// Créer des boutons radio devant chaque réponse
+// Y construire le code HTML des questions+réponses
 
 
 class Question{
@@ -66,41 +104,55 @@ class Question{
             this.answers=answers;
             this.rightAnswer=rightAnswer;
             this.comment=comment;
-            this.toHTML();
-        };
-        toHTML()
-        {
-            let html =
-                `<fieldset>
-                <legend class="legend">${this.content}</legend>
-                `;
-            for(let i=0; i<this.answers.length;i++)
-            {
-                html+=
-                    `<div class="answers">
-                    <input type="radio" name="" id="answer"> 
-                    <label for="answer">${this.answers[i]}</label>
-                    </div>`;
-            };
-
-            html+=
-                `<p class="hidden">${this.comment}</p>
-                </fieldset>`;
-            const form=document.body.querySelector("#form");
-            form.innerHTML+=html;
-        };
+        };   
 };
 
 
+// Des boutons radio à écouter
+const choice=document.querySelector('#answer');
 
+// Un bouton "submit"
+const submit=document.querySelector('#button');
+
+
+
+
+// **********************************
 // ********* CODE PRINCIPAL *********
 // **********************************
 
-// 
+// A faire:
+
+// Afficher les questions de manière aléatoire
+// Afficher les réponses de manière aléatoire
+// Placer un écouteur sur les boutons radio
+// Placer un écouteur sur le bouton "submit"
+
 
 questions=questions.map(question => {
     return new Question(question.content,question.answers,question.rightAnswer,question.comment)
 });
+
+
+let questionsCopy=shuffle(questions);
+for(let i=0;i<questionsCopy.length;i++)
+{
+    toHTML(questionsCopy[i]);
+};
+
+
+// let answersCopy=shuffle(question.answers);
+
+
+// for(let i=0;i<answersCopy.length;i++)
+// {
+//     toHTML(answersCopy[i]);
+// };
+
+console.log(questions);
+console.log(questionsCopy);
+
+
 
 
 
